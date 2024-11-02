c69a0ddc5f2a8cc115d490b889b8899a392bb4d2
alwaysblue930 b <alwaysblue930@gmail.com>
2024-11-02 03:59:00 +0000

    feat: add guess menu

--- number_guess.sh
+++ number_guess.sh
@@ -2,12 +2,15 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+CURRENT_USER=""
+
 USER_MENU(){
   if [[ -n $1 ]]; then
     echo "$1"
   fi
   echo -e "\nEnter your username:"
   read INPUT
+  CURRENT_USER=$INPUT
   if [[ ${#INPUT} -le 22 ]]; then
     USER_EXISTS=$($PSQL "SELECT * FROM users WHERE username='$INPUT';")
     if [[ -z $USER_EXISTS ]]; then
@@ -16,6 +19,7 @@ USER_MENU(){
           USER_MENU "Failed to create a user with username $INPUT"
         else
           echo "Welcome, $INPUT! It looks like this is your first time here."
+          USERNAME="$INPUT"
         fi
     else
       USER_INFO=$($PSQL "SELECT username, COUNT(*), MIN(guesses) FROM users INNER JOIN games USING(username) WHERE username='$INPUT' GROUP BY username;")
@@ -29,4 +33,30 @@ USER_MENU(){
   fi
 }
 
+GOAL=$(( RANDOM % 1000 + 1))
+TRIES=1
+
+GUESS_MENU(){
+  if [[ -n $1 ]]; then
+    echo "$1"
+  fi
+
+  echo "Guess the secret number between 1 and 1000:"
+  read GUESS
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
+    GUESS_MENU "That is not an integer, guess again:"
+  else
+    ((TRIES++))
+    if (( GUESS == GOAL)); then
+      echo "You guessed it in $TRIES tries. The secret number was $GOAL. Nice job!"
+      GAME_RECORDED=$($PSQL "INSERT INTO games(username, guesses) VALUES('$CURRENT_USER', $TRIES);")
+    elif (( GUESS < GOAL )); then
+      GUESS_MENU "It's higher than that, guess again:"
+    else
+      GUESS_MENU "It's lower than that, guess again:"
+    fi
+  fi
+}
+
 USER_MENU
+GUESS_MENU
